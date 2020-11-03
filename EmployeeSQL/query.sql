86ba1166e689850ffad6f72bf4708fe839443486
Beverly Thompson <blt1339@gmail.com>
2020-11-03 13:52:59 -0500

    Add SQL for question # 5.

--- EmployeeSQL/query.sql
+++ EmployeeSQL/query.sql
@@ -55,3 +55,19 @@ LEFT OUTER JOIN dept_emp ON
 	employees.emp_no = dept_emp.emp_no
 LEFT OUTER JOIN departments ON
 	dept_emp.dept_no = departments.dept_no;
+
+
+-- 5. List first name, last name, and sex for employees whose first name is "Hercules" 
+-- and last names begin with "B."
+
+SELECT 
+	last_name,
+	first_name,
+	sex 
+FROM 
+	employees
+WHERE 
+	first_name = 'Hercules'
+AND
+	last_name LIKE 'B%';
+
