12124ee4bf46e1335dce867a13904fb0eb6816f7
Beverly Thompson <blt1339@gmail.com>
2020-11-03 13:48:34 -0500

    Add SQL for question # 2.

--- EmployeeSQL/query.sql
+++ EmployeeSQL/query.sql
@@ -11,5 +11,17 @@ FROM
 	employees
 LEFT OUTER JOIN salaries ON
 	employees.emp_no = salaries.emp_no;
+	
+	
+-- 2. List first name, last name, and hire date for employees who were hired in 1986.
+
+SELECT 
+	first_name, 
+	last_name, 
+	hire_date 
+FROM 
+	employees
+WHERE 
+	DATE_PART('year', hire_date) = '1986';
 
 
