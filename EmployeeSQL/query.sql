5ec8bd4d53e762010dc2e3313a111be9b02a8f52
Beverly Thompson <blt1339@gmail.com>
2020-11-03 13:49:52 -0500

    Add SQL for question # 3.

--- EmployeeSQL/query.sql
+++ EmployeeSQL/query.sql
@@ -24,4 +24,19 @@ FROM
 WHERE 
 	DATE_PART('year', hire_date) = '1986';
 
+-- 3. List the manager of each department with the following information: 
+-- department number, department name, the manager's employee number, last name, first name.
+
+SELECT 
+	dept_manager.dept_no,
+	departments.dept_name,
+	dept_manager.emp_no,
+	employees.last_name,
+	employees.first_name
+FROM 
+	dept_manager
+LEFT OUTER JOIN departments ON
+	dept_manager.dept_no = departments.dept_no
+LEFT OUTER JOIN employees ON
+	dept_manager.emp_no = employees.emp_no;
 
