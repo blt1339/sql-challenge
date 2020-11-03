332944181d5db7e6a92ecc44c39e51458177c834
Beverly Thompson <blt1339@gmail.com>
2020-11-03 13:51:21 -0500

    Add SQL for question #4.

--- EmployeeSQL/query.sql
+++ EmployeeSQL/query.sql
@@ -40,3 +40,18 @@ LEFT OUTER JOIN departments ON
 LEFT OUTER JOIN employees ON
 	dept_manager.emp_no = employees.emp_no;
 
+
+-- 4. List the department of each employee with the following information: 
+-- employee number, last name, first name, and department name.
+
+SELECT 
+	employees.emp_no,
+	employees.last_name,
+	employees.first_name,
+	departments.dept_name
+FROM 
+	employees
+LEFT OUTER JOIN dept_emp ON
+	employees.emp_no = dept_emp.emp_no
+LEFT OUTER JOIN departments ON
+	dept_emp.dept_no = departments.dept_no;
