b6d41ed7c387d0b3092627140f308cf301fe0a40
Beverly Thompson <blt1339@gmail.com>
2020-11-08 15:59:59 -0500

    Add comments and finalize readme.

---
+++ EmployeeSQL/EmployeeSQL Creation.sql
@@ -0,0 +1,94 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Link to schema: https://app.quickdatabasediagrams.com/#/d/mcYu7k
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+-----------------
+-- sql-challenge
+-----------------
+
+
+-- Drop the tables
+DROP TABLE if exists departments cascade;
+DROP TABLE if exists dept_emp cascade;
+DROP TABLE if exists dept_manager cascade;
+DROP TABLE if exists employees cascade;
+DROP TABLE if exists salaries cascade;
+DROP TABLE if exists titles cascade;
+
+-- Create departments
+CREATE TABLE "departments" (
+    "dept_no" VARCHAR(10)   NOT NULL,
+    "dept_name" VARCHAR(25)   NOT NULL,
+    CONSTRAINT "pk_departments" PRIMARY KEY (
+        "dept_no"
+     )
+);
+
+-- Create dept_emp
+CREATE TABLE "dept_emp" (
+    "emp_no" INT   NOT NULL,
+    "dept_no" VARCHAR(10)   NOT NULL,
+    CONSTRAINT "pk_dept_emp" PRIMARY KEY (
+        "emp_no","dept_no"
+     )
+);
+
+-- Create dept_manager
+CREATE TABLE "dept_manager" (
+    "dept_no" VARCHAR(10)   NOT NULL,
+    "emp_no" INT   NOT NULL,
+    CONSTRAINT "pk_dept_manager" PRIMARY KEY (
+        "dept_no","emp_no"
+     )
+);
+
+-- Create employees
+CREATE TABLE "employees" (
+    "emp_no" INT   NOT NULL,
+    "emp_title_id" VARCHAR(20)   NOT NULL,
+    "birth_date" DATE   NOT NULL,
+    "first_name" VARCHAR(25)   NOT NULL,
+    "last_name" VARCHAR(25)   NOT NULL,
+    "sex" CHAR   NOT NULL,
+    "hire_date" DATE   NOT NULL,
+    CONSTRAINT "pk_employees" PRIMARY KEY (
+        "emp_no"
+     )
+);
+
+-- Create salaries
+CREATE TABLE "salaries" (
+    "emp_no" INT   NOT NULL,
+    "salary" INT   NOT NULL,
+    CONSTRAINT "pk_salaries" PRIMARY KEY (
+        "emp_no"
+     )
+);
+
+-- Create titles
+CREATE TABLE "titles" (
+    "title_id" VARCHAR(10)   NOT NULL,
+    "title" VARCHAR(25)   NOT NULL,
+    CONSTRAINT "pk_titles" PRIMARY KEY (
+        "title_id"
+     )
+);
+
+ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
+ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "departments" ("dept_no");
+
+ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "departments" ("dept_no");
+
+ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
+ALTER TABLE "employees" ADD CONSTRAINT "fk_employees_emp_title_id" FOREIGN KEY("emp_title_id")
+REFERENCES "titles" ("title_id");
+
+ALTER TABLE "salaries" ADD CONSTRAINT "fk_salaries_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
--- README.md
+++ README.md
@@ -4,14 +4,46 @@
 # Background
 [Project Details](project_instructions_readme.md)
 
-# Setup Instructions
-In order to run these programs, a file called config.py needs to be created in the directory EmployeeSQL with the following defined:
-user_name = ""
-password = ""
-port_no = ""
-db_name = ""
+# Project Information
+Inside the EmployeeSQL directory there are the following:
 
-db_name needs to be defined as EmployeeSQL_db.
+    * data directory - Contains the csv files for import into EmployeeSQL
 
+    * ERA_Setup.xlsx - Contains the information pulled from the CSV files to utilze when creating tables
+    * ERA_Setup.txt - Contains the information used to create the ERD Diagram
+    * ERA_Diagram.png - A ERA Diagram of EmployeeSQL
 
+    * EmployeeSQL_Creation.sql - contains the SQL to create the tables in the EmployeeSQL database
 
+    * EmployeeSQL_Query.sql - contains the SQL to run the requseted queries on EmployeeSQL database
+
+    * EmployeeSQL_Bonus_Analysis.ipynb - A Jupyter Notebook to perform the bonus analysis using SQLAlchemy 
+    * config.py - This file is necessary to execute EmployeeSQL Bonus Analysis.ipynb and must be created using the correct values instead of "".
+
+        user_name = ""
+        password = ""
+        port_no = ""
+        db_name = "EmployeeSQL"
+
+# ERD Diagram
+![ERD_Diagram.png](EmployeeSQL/images/ERD_Diagram.png)
+
+# Bonus Analysis Observations
+## The following is the common salaries analysis
+![sql.png](EmployeeSQL/images/common_salaries.png)
+
+### Observations
+The number of employees in the smaller salary ranges are greater than the number employees in the larger salary range. That is realistic.
+But according to statista.com HP did not reach over 300,000 until 2008. Additionally HP Wikipedia mentioned that there were 85,000 employees in 1985. Therefore the total salary records of 300,024 in the 80s and 90s does not seem realistic.
+
+## The following is the average salary per title analysis
+![sql.png](EmployeeSQL/images/average_salaries_per_title.png)
+
+### Observations
+There are some things that do not make sense when reviewing this result. Assistant Engineer, Engineer, and Senior Engineer have average salaries that are almost the same. As well Staff and Senior Staff have around the same average salary which should not be the case.
+
+# The indentification of employee 499942
+![sql.png](EmployeeSQL/employee_499942.png)
+
+# Observations
+Given the name of the employee is April Foolsday, the reference to this employee seems to confirm that this assignement was a test.
